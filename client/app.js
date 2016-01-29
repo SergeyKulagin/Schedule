7987448e1dc80d39594f96cfb0734b9d3ced319f
unknown <sergey.kulagin@SERGEYKULAGIN.hgrupp.ee>
2016-01-29 15:18:09 +0300

    client: AddUpdateSchedule component in progress

--- client/app.js
+++ client/app.js
@@ -1,6 +1,16 @@
+var App = React.createClass({
+	render: function () {
+		return <AddUpdateSchedule/>;
+	}
+});
+
+
 var AddUpdateSchedule = React.createClass({
+	getInitialState: function(){
+		return {activities:[]};
+	},
 	render: function(){
-		return <div className="container add-or-update-schedule-container">
+		return <div className="add-or-update-schedule-container">
 			<div className="row">
 			    <label className="col-md-6">Schedule name</label>
 				<input className="col-md-6" type="text" name="scheduleName"/>
@@ -9,17 +19,108 @@ var AddUpdateSchedule = React.createClass({
 			    <label className="col-md-6">Start date</label>
 				<input className="col-md-6" type="date" name="startDate"/>
 			</div>
-			<div>
+			<ActivityItems activities={this.state.activities}/>	
+			<div className="row">
+				  <div className="schedule-item-add-btn" onClick={this.onPeriodAdd}/>
+			</div>
+			<div className="row">
 				<input type="button" name="save" value="Save"/>
 			</div>
 		</div>;
+	},
+	onPeriodAdd: function(event) {
+		var defaultActivity = {
+			name: "Default",
+			color: "",
+			days: []
+		};
+		var activities = this.state.activities;
+		activities.push(defaultActivity);
+    	this.setState({activities:activities});
+  	},
+});
+
+var ActivityItems = React.createClass({
+	render: function(){
+		var activities = this.props.activities;
+		if(!activities){
+			return <div/>;
+		}
+		var activitiesWidgets = [];
+		for (var i = activities.length - 1; i >= 0; i--) {
+			var activity = activities[i];
+			activitiesWidgets.push(<ActivityItem model={activity} id={activity.id}/>);
+		};
+		return <div>{activitiesWidgets}</div>;
 	}
 });
 
 
-var App = React.createClass({
-	render: function () {
-		return <AddUpdateSchedule/>;
+var ActivityItem =  React.createClass({
+	getInitialState: function(){
+		return {
+			name: this.props.model.name
+		};
+	},
+	componentDidMount: function(){
+		$('select[name="colorpicker"]').simplecolorpicker();
+	},
+	render: function(){
+		return <div className="activity-item">
+		          <div className="row">
+		          	<input className="col-md-6 bold" value={this.state.name} onChange={this.onNameChange}></input>
+		          </div>
+		            <div className="row">
+		               <div className="col-md-6"><label >Color</label></div>
+		                <div className="col-md-6">
+		                  <select className="colorPicker" name="colorpicker">
+								<option value="#7bd148">Green</option>
+								<option value="#5484ed">Bold blue</option>
+								<option value="#a4bdfc">Blue</option>
+								<option value="#46d6db">Turquoise</option>
+								<option value="#7ae7bf">Light green</option>
+								<option value="#51b749">Bold green</option>
+								<option value="#fbd75b">Yellow</option>
+								<option value="#ffb878">Orange</option>
+								<option value="#ff887c">Red</option>
+								<option value="#dc2127">Bold red</option>
+								<option value="#dbadff">Purple</option>
+								<option value="#e1e1e1">Gray</option>	            				   
+				  		   </select>
+				  		   </div>	
+		          </div>		          		          
+		          <div className="col-md-6">
+		          	<ScheduleItemPeriods className="col-md-5" items={this.props.items}/>
+		          </div>		      	  
+		    	</div>;	
+	},
+	onNameChange: function(){
+		 this.setState({name: event.target.value});
+	}
+});
+
+var ScheduleItemPeriods = React.createClass({
+	render: function(){
+		var items = this.props.items;
+		if(!items){
+			return <div/>;
+		}
+		var itemsWidgets = [];
+		for (var i = items.length - 1; i >= 0; i--) {
+			var item = items[i];
+			itemsWidgets.push(<ScheduleItemPeriod id={this.props.day}/>);
+		};
+		return <div>{itemsWidgets}</div>;
+	}
+});
+
+var ScheduleItemPeriod = React.createClass({
+	render: function(){		
+		return <div>
+					
+					<label>Days</label>	
+					<input></input>
+				</div>;
 	}
 });
 
@@ -27,3 +128,5 @@ ReactDOM.render(
   <App/>,
   document.getElementById('mainPage')
 );
+
+
